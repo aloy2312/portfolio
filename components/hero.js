628ac5d50d77019653d58cd46dab1770245c6450
aloy2312 <aloysiustanrs@gmail.com>
2022-04-10 16:13:02 +0800

    update font sizes and text structure for hero

--- components/hero.js
+++ components/hero.js
@@ -4,7 +4,9 @@ const hero = () => {
   return (
     <div className={styles.container}>
       <div className={styles.textbox}>
-        <div className={styles.text1}>Hi , my name is Aloysius Tan</div>
+        <div className={styles.text1}>
+          Hi , my name is <br className={styles.br} /> Aloysius Tan
+        </div>
         <hr className={styles.horizontal}></hr>
         <div className={styles.text2}>I{`'`}m a Software Engineer</div>
       </div>
--- components/projects.js
+++ components/projects.js
@@ -5,7 +5,10 @@ const projects = () => {
   return (
     <div className={styles.container}>
       <div className={styles.projectsTitle}>Projects</div>
-      <div className="projectGroup"></div>
+      <div className="projectGroup">
+        <div className="projectText"></div>
+        <div className="projectImage"></div>
+      </div>
     </div>
   );
 };
