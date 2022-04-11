2ee59b78a1827bca80582eef09cc6e480f867da0
aloy2312 <aloysiustanrs@gmail.com>
2022-04-11 17:49:42 +0800

    contact section added

--- components/contact.js
+++ components/contact.js
@@ -1,11 +1,43 @@
 import React from "react";
 import styles from "../styles/contact.module.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
+import {
+  faGithub,
+  faLinkedin,
+  faMedium,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons"; // import the icons you need
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 const contact = () => {
   return (
     <div className={styles.container}>
       <div className={styles.contactTitle}>Contact Me</div>
-      <div></div>
+      <div className={styles.contactTexts}>
+        <p className={styles.contactMessage}>
+          Feel free to reach out to me at any time!
+        </p>
+        <p className={styles.contactName}>Aloysius Tan</p>
+        <a className={styles.contactEmail}>aloysiustanrs@gmail.com</a>
+      </div>
+      <div className={styles.contactButtons}>
+        <button className={styles.contactButton}>
+          <FontAwesomeIcon className={styles.icon} icon={faLinkedin} /> Linkedin
+        </button>
+        <button className={styles.contactButton}>
+          <FontAwesomeIcon className={styles.icon} icon={faTwitter} /> Twitter
+        </button>
+        <button className={styles.contactButton}>
+          <FontAwesomeIcon className={styles.icon} icon={faGithub} /> Github
+        </button>
+        <button className={styles.contactButton}>
+          <FontAwesomeIcon className={styles.icon} icon={faMedium} /> Medium
+        </button>
+        <button className={styles.contactButton}>
+          <FontAwesomeIcon className={styles.icon} icon={faEnvelope} /> Mail
+        </button>
+      </div>
     </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import "../styles/globals.css";
 
+// Font Awesome
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
